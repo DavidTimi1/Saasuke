e84317aba33c0f36c0b6147b467ac944ad7b2bee
David <duwagbale07@gmail.com>
2024-11-02 04:19:09 +0100

    fix(env): fixed env vars name bug

--- main_auth/settings.py
+++ main_auth/settings.py
@@ -7,9 +7,9 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 # Google OAuth2 Settings
-GOOGLE_OAUTH2_ID = os.environ.get("google-client-id")
-GOOGLE_OAUTH2_SECRET = os.environ.get("google-client-secret")
+GOOGLE_OAUTH2_ID = os.environ.get("google_client_id")
+GOOGLE_OAUTH2_SECRET = os.environ.get("google_client_secret")
 
 # Facebook OAuth2 Settings
-FACEBOOK_KEY = os.environ.get("fb-app-id")
-FACEBOOK_SECRET = os.environ.get("fb-app-secret")
+FACEBOOK_KEY = os.environ.get("fb_app_id")
+FACEBOOK_SECRET = os.environ.get("fb_app_secret")
--- tg_bot/settings.py
+++ tg_bot/settings.py
@@ -2,11 +2,11 @@ import os
 
 HOST = os.environ.get("host")
 
-bot_token = os.environ.get("tg-bot-token")
-hook_secret = os.environ.get("tg-webhook-secret")
-hook_url = HOST + "/tg-bot/webhook"
+bot_token = os.environ.get("tg_bot_token")
+hook_secret = os.environ.get("tg_webhook_secret")
+hook_url = HOST + "/tg_bot/webhook"
 
-gemini_api = os.environ.get("gemini-api")
+gemini_api = os.environ.get("gemini_api")
 
 landing_page = "https://davidtimi.pythonanywhere.com/botspage"
 mini_app = "t.me/Beetcoin_Bbot/lfgtelegram"
